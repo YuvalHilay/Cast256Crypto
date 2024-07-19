5c3640c5886fc3eb1d514599383220dfe53ef9c5
YuvalHilay <73775490+YuvalHilay@users.noreply.github.com>
2024-07-19 14:53:52 +0300

    Create mac.py

---
+++ mac.py
@@ -0,0 +1,27 @@
+import hashlib
+import hmac
+import os
+
+def generate_mac(padded_message):
+    # generate random key (256-bit for CAST256 consistency)
+    mac_key = os.urandom(32)  # 32 bytes = 256 bits
+    if isinstance(mac_key, str):
+        mac_key = mac_key.encode('utf-8')
+    if isinstance(padded_message, str):
+        padded_message = padded_message.encode('utf-8')
+
+    hmac_calculated = hmac.new(mac_key, padded_message, hashlib.sha256)
+    return mac_key, hmac_calculated.digest()
+
+def verify_mac(Dec_Data, mac, mac_key):
+    if isinstance(mac_key, str):
+        mac_key = mac_key.encode('utf-8')
+    if isinstance(Dec_Data, str):
+        Dec_Data = Dec_Data.encode('utf-8')
+    hmac_calculated = hmac.new(mac_key, Dec_Data, hashlib.sha256)
+    hmac_digest = hmac_calculated.digest()
+    print("MAC for the decrypted image:", hmac_digest)
+    if hmac.compare_digest(mac, hmac_digest):
+        print("\nMAC verification successful.")
+    else:
+        print("\nMAC verification failed.")
